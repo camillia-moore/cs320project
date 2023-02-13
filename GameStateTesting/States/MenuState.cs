d0f5cd8205f2a60876be98a914916228f3c2a939
camillia-moore <122479807+camillia-moore@users.noreply.github.com>
2023-02-13 11:35:42 -0800

    Got buttons working
    some of lyndsey's code's been factored out
    also, to get back to menu state from battle state, you must press p, not up

--- GameStateTesting/States/MenuState.cs
+++ GameStateTesting/States/MenuState.cs
@@ -8,18 +8,88 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using GameStateTesting.States;
+using Myra;
+using Myra.Graphics2D.UI;
 
 namespace GameStateTesting.States
 {
     public class MenuState : State
     {
+        private Desktop _desktop;
         public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
         {
+            
         }
 
         public override void LoadContent()
         {
-            //throw new NotImplementedException();
+            MyraEnvironment.Game = _game;
+
+            var grid = new Grid
+            {
+                RowSpacing = 8,
+                ColumnSpacing = 8
+            };
+
+            grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
+            grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
+            grid.RowsProportions.Add(new Proportion(ProportionType.Auto));
+            grid.RowsProportions.Add(new Proportion(ProportionType.Auto));
+
+            // Button to Character Creation
+            var buttonCCS = new TextButton
+            {
+                GridColumn = 0,
+                GridRow = 0,
+                Text = "Character Creation State"
+            };
+
+            buttonCCS.Click += (s, a) =>
+            {
+                /*var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
+                messageBox.ShowModal(_desktop);*/
+                _game.ChangeState(new CharacterCreationState(_game, _graphicsDevice, _content));
+            };
+
+            grid.Widgets.Add(buttonCCS);
+
+            // Button to Story State
+            var buttonSS = new TextButton
+            {
+                GridColumn = 0,
+                GridRow = 1,
+                Text = "Story State"
+            };
+
+            buttonSS.Click += (s, a) =>
+            {
+                /*var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
+                messageBox.ShowModal(_desktop);*/
+                _game.ChangeState(new StoryState(_game, _graphicsDevice, _content));
+            };
+
+            grid.Widgets.Add(buttonSS);
+
+            // Button to Battle State
+            var buttonBS = new TextButton
+            {
+                GridColumn = 0,
+                GridRow = 2,
+                Text = "Battle State"
+            };
+
+            buttonBS.Click += (s, a) =>
+            {
+                /*var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
+                messageBox.ShowModal(_desktop);*/
+                _game.ChangeState(new BattleState(_game, _graphicsDevice, _content));
+            };
+
+            grid.Widgets.Add(buttonBS);
+
+            // Add it to the desktop
+            _desktop = new Desktop();
+            _desktop.Root = grid;
         }
 
         public override void Update(GameTime gameTime)
@@ -53,6 +123,9 @@ namespace GameStateTesting.States
         {
             //throw new NotImplementedException();
             _graphicsDevice.Clear(Color.CornflowerBlue);
+            _desktop.Render();
         }
+
+
     }
 }
