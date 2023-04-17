99d98ed95c52323d9601f74ca9c4c2dea6c17488
camillia-moore <122479807+camillia-moore@users.noreply.github.com>
2023-04-16 20:42:55 -0700

    Started overhaul of menu system
    Replaced the Myra based menu on the main menu with a simple text and keyboard based one, based on June's code

--- GameStateTesting/States/MenuState.cs
+++ GameStateTesting/States/MenuState.cs
@@ -8,132 +8,76 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using GameStateTesting.States;
-using Myra;
-using Myra.Graphics2D.UI;
 
 namespace GameStateTesting.States
 {
     public class MenuState : State
     {
-        private Desktop _desktop;
         private Texture2D titleScreen;
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        private SpriteFont font;
+        private int optionFocused;
+        private KeyboardState oldKstate;
         public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
         {
-            
+            optionFocused = 0;
         }
 
         public override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(_graphicsDevice);
             titleScreen = _content.Load<Texture2D>("cough-story-title-draft");
-
-            MyraEnvironment.Game = _game;
-
-            var grid = new Grid
-            {
-                RowSpacing = 8,
-                ColumnSpacing = 8,
-                Top = 270,
-                Left = 920
-            };
-
-            grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
-            grid.ColumnsProportions.Add(new Proportion(ProportionType.Auto));
-            grid.RowsProportions.Add(new Proportion(ProportionType.Auto));
-            grid.RowsProportions.Add(new Proportion(ProportionType.Auto));
-
-            // Button to Character Creation
-            var buttonCCS = new TextButton
-            {
-                GridColumn = 0,
-                GridRow = 0,
-                Text = "Character Creation State"
-            };
-
-            buttonCCS.Click += (s, a) =>
-            {
-                /*var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
-                messageBox.ShowModal(_desktop);*/
-                _game.ChangeState(new CharacterCreationState(_game, _graphicsDevice, _content));
-            };
-
-            grid.Widgets.Add(buttonCCS);
-
-            // Button to Story State
-            var buttonSS = new TextButton
-            {
-                GridColumn = 0,
-                GridRow = 1,
-                Text = "Story State"
-            };
-
-            buttonSS.Click += (s, a) =>
-            {
-                /*var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
-                messageBox.ShowModal(_desktop);*/
-                _game.ChangeState(new StoryState(_game, _graphicsDevice, _content));
-            };
-
-            grid.Widgets.Add(buttonSS);
-
-            // Button to Battle State
-            var buttonBS = new TextButton
-            {
-                GridColumn = 0,
-                GridRow = 2,
-                Text = "Battle State"
-            };
-
-            buttonBS.Click += (s, a) =>
-            {
-                /*var messageBox = Dialog.CreateMessageBox("Message", "Some message!");
-                messageBox.ShowModal(_desktop);*/
-                BattleState nextState = new BattleState(_game, _graphicsDevice, _content);
-                nextState.createPlayer("Menu's KitKat", "The Default Hero", 30, 9, 5, 10);
-                nextState.addSpell("Menu's Fireball", "Deals damage to the opponent", -10, 0, 0, 1, 3);
-                nextState.addSpell("Ice Storm", "Uses Ice to Weaken the enemy", 0, -2, -2, 1, 4);
-                nextState.addSpell("Diacute", "Buffs the user's stats", 0, +2, +2, 0, 5);
-                nextState.addSpell("Menu's Healing", "Heals the user", +5, 0, 0, 0, 6);
-                nextState.buffPlayer(+0, +1, +0, +0); //should buf attack by 1
-                nextState.fromMenu(true);//then swap to the battle
-                _game.ChangeState(nextState);
-
-            };
-
-            grid.Widgets.Add(buttonBS);
-
-            // Add it to the desktop
-            _desktop = new Desktop();
-            _desktop.Root = grid;
+            font = _content.Load<SpriteFont>("TestFont");
         }
 
         public override void Update(GameTime gameTime)
         {
             //throw new NotImplementedException();
-            var kstate = Keyboard.GetState();
+            var newKstate = Keyboard.GetState();
 
-            /*if (kstate.IsKeyDown(Keys.Up))
+            if(newKstate.IsKeyDown(Keys.Down) && oldKstate.IsKeyUp(Keys.Down))
             {
-                _game.ChangeState(new MenuState(_game, _graphicsDevice, _content));
-            }*/
-
-            if (kstate.IsKeyDown(Keys.Down))
+                optionFocused += 1;
+            }
+            if (newKstate.IsKeyDown(Keys.Up) && oldKstate.IsKeyUp(Keys.Up))
             {
-                _game.ChangeState(new StoryState(_game, _graphicsDevice, _content));
+                optionFocused += -1;
+            }
+            if (optionFocused == 3) { optionFocused = 0; }
+            if (optionFocused == -1) { optionFocused = 2; }
+            if (newKstate.IsKeyDown(Keys.Enter) || newKstate.IsKeyDown(Keys.Space)) 
+            {
+                switch (optionFocused)
+                {
+                    case 0:
+                        //go to character creation state
+                        _game.ChangeState(new CharacterCreationState(_game, _graphicsDevice, _content));
+                        break;
+                    case 1:
+                        //go to story state
+                        _game.ChangeState(new StoryState(_game, _graphicsDevice, _content));
+                        break;
+                    case 2:
+                        //go to battle state
+                        BattleState nextState = new BattleState(_game, _graphicsDevice, _content);
+                        nextState.createPlayer("Menu's KitKat", "The Default Hero", 30, 9, 5, 10);
+                        nextState.addSpell("Menu's Fireball", "Deals damage to the opponent", -10, 0, 0, 1, 3);
+                        nextState.addSpell("Ice Storm", "Uses Ice to Weaken the enemy", 0, -2, -2, 1, 4);
+                        nextState.addSpell("Diacute", "Buffs the user's stats", 0, +2, +2, 0, 5);
+                        nextState.addSpell("Menu's Healing", "Heals the user", +5, 0, 0, 0, 6);
+                        //nextState.buffPlayer(+0, +1, +0, +0); //should buf attack by 1
+                        nextState.fromMenu(true);//then swap to the battle
+                        _game.ChangeState(nextState);
+                        break;
+                    default:
+                        //do nothing
+                        break;
+                }
             }
 
-            if (kstate.IsKeyDown(Keys.Left))
-            {
-                _game.ChangeState(new CharacterCreationState(_game, _graphicsDevice, _content));
-            }
 
-            if (kstate.IsKeyDown(Keys.Right))
-            {
-                _game.ChangeState(new BattleState(_game, _graphicsDevice, _content));
-            }
-
+            oldKstate = newKstate;
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
@@ -143,11 +87,9 @@ namespace GameStateTesting.States
 
             _spriteBatch.Begin();
             _spriteBatch.Draw(titleScreen, new Vector2(0, 0), Color.White);
+            _spriteBatch.DrawString(font, "Go To Scene " + optionFocused, new Vector2(920, 570), Color.Black);
             _spriteBatch.End();
 
-            _desktop.Render();
         }
-
-
     }
 }
